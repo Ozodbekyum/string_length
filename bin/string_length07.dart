39fbc783fd5b525023f6c7b1a31edcce9243c047
Ozodbekyum <hello7ozod@gamil.com>
2022-08-02 10:14:07 +0500

    length07

--- bin/string_length07.dart
+++ bin/string_length07.dart
@@ -1,12 +1,21 @@
 /*    
     Create function func with function arguments 's1' and 's2' 
-        Given three strings, s1 and s2 . return their even lengths, example "s1". If there is no even length, return "s2".
+        Given three strings, s1 and s2 . return their even lengths, example "s1". 
+        If there is no even length, return "s2".
     Args:
         s1: string
         s2: string
     Returns:
         string
 */
+String func(String s1, String s2) {
+  if (s1.length % 2 == 0) {
+    if (s2.length % 2 == 0) {}
+  }
+  return s1;
+}
+
 void main() {
+  print(func('okk', 'code'));
   // write your code here
 }
